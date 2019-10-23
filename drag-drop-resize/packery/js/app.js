e7d9816c8e9ef33823b949a85dfa114091eea0a5
Dean Fogarty <angrytongan@gmail.com>
2019-10-23 13:46:42 +1000

    WIP

--- drag-drop-resize/packery/js/app.js
+++ drag-drop-resize/packery/js/app.js
@@ -1,7 +1,43 @@
+let grid;
+
 document.addEventListener('DOMContentLoaded', function(e) {
-    var pckry = new Packery( '.grid', {
-      itemSelector: '.grid-item',
-      columnWidth: 100
+    var pckry = new Packery('.grid', {
+        itemSelector: '.grid-item',
+        columnWidth: 100,
+    });
+
+    grid = document.querySelector('.grid');
+
+    function getItemElement() {
+        var item = document.createElement('div');
+        // add width and height class
+        var wRand = Math.random();
+        var hRand = Math.random();
+        var widthClass = wRand > 0.85 ? 'grid-item--width3' : wRand > 0.7 ? 'grid-item--width2' : '';
+        var heightClass = hRand > 0.85 ? 'grid-item--height3' : hRand > 0.5 ? 'grid-item--height2' : '';
+        item.className = 'grid-item ' + widthClass + ' ' + heightClass;
+        return item;
+    }
+
+    let append_button = document.querySelector('#append-box');
+    append_button.addEventListener('click', function() {
+        let items = [
+            getItemElement(),
+            getItemElement(),
+            getItemElement(),
+        ];
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(items[0]);
+        fragment.appendChild(items[1]);
+        fragment.appendChild(items[2]);
+        grid.appendChild(fragment);
+
+        pckry.appended(items);
+
+        items.forEach(function(el) {
+            let draggie = new Draggabilly(el);
+            pckry.bindDraggabillyEvents(draggie);
+        });
     });
 
     pckry.getItemElements().forEach( function( itemElem ) {
