f5de61752426b21d2589d8dbd17e79c55af38a41
Dean Fogarty <angrytongan@gmail.com>
2019-10-25 19:18:42 +1000

    WIP

--- graphs/amcharts/js/app.js
+++ graphs/amcharts/js/app.js
@@ -1,5 +1,14 @@
 let chart;
-let series;
+let timerId = 0;
+
+let randomData = function() {
+    chart.addData([
+        {
+            country: "XX" + Math.floor(Math.random() * 10000),
+            litres: 5 + Math.floor(Math.random() * 10)
+        }
+    ], (Math.random() > 0.5) ? 1 : (chart.data.length > 20 ? 2 : 0));
+};
 
 document.addEventListener('DOMContentLoaded', function(e) {
     chart = am4core.createFromConfig({
@@ -10,35 +19,51 @@ document.addEventListener('DOMContentLoaded', function(e) {
                 category: "country"
             }
         }],
-        data: [{
-          "country": "Lithuania",
-          "litres": 501.9
-        }, {
-          "country": "Czech Republic",
-          "litres": 301.9
-        }, {
-          "country": "Ireland",
-          "litres": 201.1
-        }, {
-          "country": "Germany",
-          "litres": 165.8
-        }, {
-          "country": "Australia",
-          "litres": 139.9
-        }, {
-          "country": "Austria",
-          "litres": 128.3
-        }, {
-          "country": "UK",
-          "litres": 99
-        }, {
-          "country": "Belgium",
-          "litres": 60
-        }, {
-          "country": "The Netherlands",
-          "litres": 50
-        }],
+
+        data: [
+            {
+              country: "Lithuania",
+              litres: 501.9
+            }, {
+              country: "Czech Republic",
+              litres: 301.9
+            }, {
+              country: "Ireland",
+              litres: 201.1
+            }, {
+              country: "Germany",
+              litres: 165.8
+            }, {
+              country: "Australia",
+              litres: 139.9
+            }, {
+              country: "Austria",
+              litres: 128.3
+            }, {
+              country: "UK",
+              litres: 99
+            }, {
+              country: "Belgium",
+              litres: 60
+            }, {
+              country: "The Netherlands",
+              litres: 50
+            }
+        ],
 
         legend: {}
     }, "chartdiv", am4charts.PieChart);
+
+    document.querySelector('#start').addEventListener('click', function(e) {
+        if (timerId != 0) {
+            clearInterval(timerId);
+        }
+        timerId = setInterval(randomData, 1000);
+    });
+
+    document.querySelector('#stop').addEventListener('click', function(e) {
+        if (timerId != 0) {
+            clearInterval(timerId);
+        }
+    });
 });
